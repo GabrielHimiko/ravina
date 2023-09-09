4b63a1dac751de8318930e9a31f9079e2cdabc7b
GabrielHimiko <122735381+GabrielHimiko@users.noreply.github.com>
2023-09-09 19:35:23 -0300

    Add files via upload

--- all.js
+++ all.js
@@ -1,12 +1,12 @@
 function setViewportHeight() {
     const windowHeight = window.innerHeight;
-    document.querySelector('#cellbody').style.setProperty('--vh', `${windowHeight}px`);
+    document.querySelector('main').style.setProperty('--vh', `${windowHeight}px`);
     localStorage.setItem('storedVH', `${windowHeight}px`);
 };
 
 if (localStorage.getItem('storedVH')) {
     const storedVH = localStorage.getItem('storedVH');
-    document.querySelector('#cellbody').style.setProperty('--vh', storedVH);
+    document.querySelector('main').style.setProperty('--vh', storedVH);
 } else {
     window.addEventListener('DOMContentLoaded', setViewportHeight);
 };
