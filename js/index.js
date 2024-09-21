e2815ba16155d0305d8b95928617857e9b1f2a92
GabrielHimiko <122735381+GabrielHimiko@users.noreply.github.com>
2024-09-21 03:13:55 -0300

    redirect da index pra store

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             document.querySelector('#check').classList.remove('bx-tada');
             navigator.vibrate(200);
-            //window.location.href = 'store.html';
+            window.location.href = '/store.html';
         }, 1500);
     }, 6000);
 });
