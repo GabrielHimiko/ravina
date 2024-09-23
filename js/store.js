9e7ffadc5df5aa8e53bfe543126439155944e889
GabrielHimiko <122735381+GabrielHimiko@users.noreply.github.com>
2024-09-22 23:53:16 -0300

    storeUpdate

--- js/store.js
+++ js/store.js
@@ -4,7 +4,6 @@ const topMenu = {
     prim: document.querySelector('#topMenu_prim'),
     sec: document.querySelector('#topMenu_sec'),
 }
-const topMenu_size = topMenu.cont.offsetHeight;
 const middle = document.querySelector('#middle');
 const search = {
     cont: document.querySelector('#search'),
@@ -19,8 +18,8 @@ search.inp.addEventListener('focus', () => {
     search.go.style.display = 'flex';
     search.icon.style.display = 'none';
     search.inp.style.borderWidth = '1px 0 1px 1px';
-    search.inp.style.borderRadius = '5px 0 0 5px';
-    search.inp.style.paddingLeft = '5px';
+    search.inp.style.borderRadius = '10px 0 0 10px';
+    search.inp.style.paddingInline = '10px';
     search.inp.style.borderColor = 'rgb(100, 100, 255)';
     cart.style.display = 'none';
 });
@@ -31,15 +30,18 @@ search.inp.addEventListener('blur', () => {
     cart.style = 'inherit';
 });
 
+lastScrollTop = 0;
 middle.addEventListener('scroll', () => {
     if (middle.scrollTop > 0) {
-        topMenu.cont.classList.add('float');
-        topMenu.sec.style.display = 'none';
-        middle.style.paddingTop = topMenu_size + 'px';
+        topMenu.cont.classList.add('ending');
     } 
-    else if (middle.scrollTop <= topMenu_size) {
-        topMenu.cont.classList.remove('float');
-        topMenu.sec.style = 'inherit';
-        middle.style = 'inherit';
+    else {
+        topMenu.cont.classList.remove('ending');
     }
+
+    if ((lastScrollTop > middle.scrollTop) && (middle.scrollTop <= topMenu.sec.offsetHeight/3)) {
+        middle.scrollTo({top: 0, behavior: 'smooth'});
+    }
+    lastScrollTop = middle.scrollTop;
+
 })
